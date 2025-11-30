315940f507fdf1c9d3f81456c943f6d56840feca
Shashwat Singh <shashwatanshul@gmail.com>
2025-11-30 14:17:13 +0530

    favicon error resolved

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cookieParser from "cookie-parser";
 
-import { PORT } from './config/env.js';
+import { PORT } from "./config/env.js";
 
-import userRouter from './routes/user.routes.js';
-import authRouter from './routes/auth.routes.js';
-import subscriptionRouter from './routes/subscription.routes.js';
-import connectToDatabase from './database/mongodb.js'
-import errorMiddleware from './middlewares/error.middleware.js'
-import arcjetMiddleware from './middlewares/arcjet.middleware.js'
-import workflowRouter from './routes/workflow.routes.js'
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 
@@ -18,19 +18,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(arcjetMiddleware);
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/subscriptions', subscriptionRouter);
-app.use('/api/v1/workflows', workflowRouter);
+app.get("/favicon.ico", (req, res) => res.status(204).end());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/workflows", workflowRouter);
 
 app.use(errorMiddleware);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Subscription Tracker API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API!");
 });
 
 app.listen(PORT, async () => {
-  console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+  console.log(
+    `Subscription Tracker API is running on http://localhost:${PORT}`
+  );
 
   await connectToDatabase();
 });
